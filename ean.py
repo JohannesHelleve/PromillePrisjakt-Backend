b5816439e8159ac63254724ac8e26ffb26377347
Johannes Helleve <Johanshelleve@gmail.com>
2023-10-29 15:52:59 +0100

    first init

---
+++ ean.py
@@ -0,0 +1,18 @@
+ean = {
+"Hansa Lite": "7030019532615",
+
+"Frydenlund Fatøl": "7044610048543",
+
+"Frydenlund Juicy IPA": "7044611875582",
+
+"Bacardi Lime": "5010677551397",
+
+"Frydenlund Fatøl 0.33": "7044610241289",
+
+"Schous Pilsner 6 x 0,5l, 3 l": "7044611875995",
+
+"Smirnoff Ice Original 275ml flaske": "5410316983693",
+
+"Hansa Mango Ipa 0,5lx6 boks": "7030019535692"
+}
+
---
+++ main.py
@@ -0,0 +1,64 @@
+import pymongo 
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from ean import ean
+import os
+import requests
+
+load_dotenv()
+
+# Connect to MongoDB
+client = MongoClient(os.environ.get("MONGO_URL"))
+
+db = client["okayletsgo"]
+collection = db["kjøh"]
+
+
+
+def get_product(ean):
+    url = f"https://kassal.app/api/v1/products/ean/{ean}"
+    headers = {
+        'Authorization': f'Bearer {os.environ.get("KASSAL_BEARER_TOKEN")}'
+    }
+    response = requests.get(url, headers=headers)
+    return prettify_product(response)
+
+def prettify_product(response):
+    response_json = response.json()
+    stores = []
+
+    for store in response_json['data']['products']:
+        if store['store'] is None or store['current_price'] is None:
+            continue
+
+        stores.append({
+            'storeName': store['store']['name'],
+            'storePrice': store['current_price']['price']
+        })
+
+    stores.sort(key=lambda x: x['storePrice'])
+
+    pretty_product = {
+        'ean': response_json['data']['ean'],
+        'productName': response_json['data']['products'][0]['name'],
+        'stores': stores
+    }
+
+    return pretty_product
+
+def update_product(product):
+    try:  
+        filter = {'ean': product['ean']}
+        collection.update_one(filter, {'$set': product}, upsert=True)
+        return {'status': 200}
+    except Exception as error:
+        return {'status': 500, 'body': error}
+    
+
+def update_db():
+    for code in ean.values():
+        product = get_product(code)
+        update_product(product)
+    return {'status': 200}
+
+update_db()
