b9784fef84a5af1c4895a00b393ef17780c969ae
Johannes Helleve <Johanshelleve@gmail.com>
2023-11-04 23:11:01 +0100

    added image and description

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from ean import ean
 import os
 import requests
+import time
+import json
 
 
 load_dotenv()
@@ -20,6 +22,8 @@ def get_product(ean):
         'Authorization': f'Bearer {os.environ.get("KASSAL_BEARER_TOKEN")}'
     }
     response = requests.get(url, headers=headers)
+    test = json.dumps(response.json(), indent=4)
+
     return prettify_product(response)
 
 def is_more_than_30_days_ago(date_string):
@@ -32,12 +36,21 @@ def is_more_than_30_days_ago(date_string):
 def prettify_product(response):
     response_json = response.json()
     stores = []
+    description = None
 
     for store in response_json['data']['products']:
+
         if store['store'] is None:
             continue
+
         if is_more_than_30_days_ago(store['current_price']['date']):
             continue
+
+        if store['description'] is not None:
+            description = store['description']
+            if description == '':
+                description = None
+
 
         stores.append({
             'storeName': store['store']['name'],
@@ -53,7 +66,9 @@ def prettify_product(response):
     pretty_product = {
         'ean': response_json['data']['ean'],
         'productName': response_json['data']['products'][0]['name'],
-        'stores': stores
+        'stores': stores,
+        'image': response_json['data']['products'][0]['image'],
+        'description': description
     }
 
     return pretty_product
@@ -70,14 +85,16 @@ def update_product(product):
 def update_db():
     x = 1
     for code in ean.values():
+        if x % 60 == 0:
+            print('sleeping')
+            time.sleep(60)
+            print('waking up')
+        x += 1
         product = get_product(code)
         print(product)
-        print(update_product(product).get('status'))
-        print(x)
-        x += 1
         if product is None:
             continue
-        
+        update_product(product).get('status')
     return {'status': 200}
 
 update_db()
